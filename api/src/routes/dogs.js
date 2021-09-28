958ebeddebec836656d3564181877185b10851ab
LucianaHer <lucianaher74@gmail.com>
2021-09-28 15:46:48 -0300

    Back - Ruta /dogs hecha

--- api/src/routes/dogs.js
+++ api/src/routes/dogs.js
@@ -8,47 +8,84 @@ const {
   
 const { Dog, Temperament } = require('../db');  // me traigo los modelos
 
+///////FUNCIONES////////////////////////////
 const getInfoAPI = async () => {  // Fc para obtener todas las razas de la API
-    const urlApi= await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${YOUR_API_KEY}`);
-    
-    const infoApi = urlApi.data.map(el => {
-        return {
-            id: el.id,
-            name: el.name,
-            altura: el.height.metric,
-            image: el.image.url,
-            vida: el.life_span,
-            temperament: el.temperament,
-            peso: el.weight.metric,
-            origen: el.origin
-        }
-    })
-    return infoApi;
+    try {
+        
+        const urlApi= await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${YOUR_API_KEY}`);
+        
+        const infoApi = urlApi.data.map(el => {
+            return {
+                id: el.id,
+                name: el.name,
+                altura: el.height.metric,
+                image: el.image.url,
+                vida: el.life_span,
+                temperament: el.temperament,
+                peso: el.weight.metric,
+                origen: el.origin
+            }
+        })
+        return infoApi;
+
+    } catch (e) {
+        return('No se pudo conectar a la API',e)
+    }    
 }
 
 const getDBInfo = async () => {     // fc para obtener todos las razas de la B Datos, junto con los temperamentos
-    const dogsDB =  await Dog.findAll({
-        include: Temperament
-    });
-    return dogsDB;    
+    try {
+        
+        const dogsDB =  await Dog.findAll({
+            include: Temperament
+        });
+        return dogsDB;    
+    } catch (e) {
+        return('No se pudo acceder a la BD',e)        
+    }
 }
 
 const getAllData = async () => {
-    const apiInfo= await getInfoAPI();
-    const dbInfo= await getDBInfo();
-    const allInfo= apiInfo.concat(dbInfo);
-    return allInfo;
+    try {
+        const apiInfo= await getInfoAPI();
+        const dbInfo= await getDBInfo();
+        const allInfo= apiInfo.concat(dbInfo);
+        return allInfo;
+        
+    } catch (e) {
+        return ('error en la obtencion de datos',e)
+    }
 }
 
-//Rutas
+ async function getOneDogById(idRaza){   // funcion que busca una raza x id
 
-router.get('/', async (req,res) =>{
+    var oneDog={};
+    var allDogs= await getAllData();
+    
+    for(var i=0; i< allDogs.length; i++){
+        if (allDogs[i].id === Number(idRaza)){
+            let dog={
+                name: allDogs[i].name,
+                temperament: allDogs[i].temperament,
+                image: allDogs[i].image,
+                altura: allDogs[i].altura,
+                peso: allDogs[i].peso,
+                vida: allDogs[i].vida
+            }
+            return dog;
+        }
+    }        
+
+}
+
+////////////RUTAS////////////////////////
+
+router.get('/', async (req,res) =>{  //   RUTA /dogs ( total y x query name)
     const {name}= req.query;   // nombre de la raza por query!!!
        
-    const allData= await getAllData();
-    
+    const allData= await getAllData();   
 
-    const dataPpal = await allData.map(el => {
+    const dataPpal = await allData.map(el => {  // para devolver solo la info de la ruta ppal
         return {
             name: el.name,
             temperament: el.temperament,
@@ -69,6 +106,25 @@ router.get('/', async (req,res) =>{
 
     }  
 })
+
+router.get('/:idRaza', async (req,res)=> {  // ruta para encontrar una raza en particular (el front me manda la id), 
+                                            //pero en la Api se puede buscar x nombre(no voy a usar esa busqueda)
+    const {idRaza}=req.params;
+    
+    try {
+        var oneDog= await getOneDogById(idRaza);
+        if (oneDog){
+            res.json(oneDog);
+        }else {
+            res.send('Raza no encontrada')
+        }      
+    } catch (e) {
+        res.status(404).send('No se pudo acceder a los datos')
+    }
+})
+
+
+
 
 
 
